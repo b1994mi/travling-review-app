b94885130e0c8fd3c20476fc8526505402b56e23
b1994mi <b1994mi@gmail.com>
2021-01-15 17:57:52 +0700

    Change style to floating labels

--- src/components/ReviewForm.vue
+++ src/components/ReviewForm.vue
@@ -15,20 +15,29 @@
         />
       </div>
     </div>
-    <input
-      class="mb-3"
-      type="text"
-      placeholder="Tulis Nama Kamu"
-      required
-      v-model="nama"
-    />
-    <textarea
-      class="mb-3"
-      form="form-utama"
-      placeholder="Tulis Review Terbaikmu"
-      required
-      v-model="review"
-    />
+    <div class="form-floating mb-3">
+      <input
+        id="nama-pengulas"
+        class="form-control"
+        type="text"
+        placeholder="Nama Lengkap"
+        required
+        v-model="nama"
+      />
+      <label for="nama-pengulas">Nama Lengkap</label>
+    </div>
+    <div class="form-floating">
+      <textarea
+        id="isi-ulasan"
+        class="mb-3 form-control"
+        style="height:6rem"
+        form="form-utama"
+        placeholder="Tulis Review Terbaikmu"
+        required
+        v-model="review"
+      />
+      <label for="isi-ulasan">Tulis Review Terbaikmu</label>
+    </div>
     <div class="d-flex justify-content-evenly flex-wrap">
       <input type="file" nama="images" id="unggah-file" multiple />
       <input type="submit" value="Kirim" />
