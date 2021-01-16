420ea3873e3092e8217a04b3f015b2c7534928a7
b1994mi <b1994mi@gmail.com>
2021-01-16 23:36:43 +0700

    Pindahkan Bootstrap CDN ke index.html

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,4 @@
 <template>
-  <!-- Bootstrap CSS CDN -->
-  <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-    crossorigin="anonymous"
-  />
-
   <!-- Form to input and post request review to DB -->
   <review-form @suksesTambah="pushReviewToArr" />
 
